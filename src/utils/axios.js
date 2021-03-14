1d32e58b28686ffa4c323ba5b7a10edcfb487415
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2021-03-14 12:30:39 +0300

    docs: update docs for axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * Create an axios instance
+ * @function
+ * @params {object} baseURL: value
+ */
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/",
 });
