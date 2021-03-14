dc655d4c235a433fbaf85a29e2fec50694d0f511
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2021-03-14 11:41:38 +0300

    test: test app component

---
+++ tests/unit/App.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+import App from "../../src/App.vue";
+
+describe("App", () => {
+  it("it works", () => {
+    const wrapper = shallowMount(App);
+  });
+});
