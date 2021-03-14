4d12bf63302c403e839d77ed334f0726a0fbd9ff
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2021-03-14 19:24:49 +0300

    test: more testing for custom btn group

--- tests/unit/components/CustomBtnGroup.spec.js
+++ tests/unit/components/CustomBtnGroup.spec.js
@@ -17,5 +17,40 @@ describe("CustomBtnGroup", () => {
     expect(wrapper.props().content.path).toBe("https://www.toolio.com");
     expect(wrapper.props().content.title).toBe("Visit the actual page");
     expect(wrapper.props().content.icon).toBe("mdi-open-in-new");
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("renders a message and responds correctly to user input", () => {
+    const wrapper = mount(CustomBtnGroup, {
+      propsData: {
+        content: {
+          path: "",
+          title: "---",
+          icon: "###",
+        },
+      },
+    });
+
+    // see if the props are rendered correctly
+    expect(wrapper.props().content.path).toBe("");
+    expect(wrapper.props().content.title).toBe("---");
+    expect(wrapper.props().content.icon).toBe("###");
+  });
+
+  it("renders a message and responds correctly to user input", () => {
+    const wrapper = mount(CustomBtnGroup, {
+      propsData: {
+        content: {
+          path: "www.google.com",
+          title: "Google",
+          icon: "",
+        },
+      },
+    });
+
+    // see if the props are rendered correctly
+    expect(wrapper.props().content.path).toBe("www.google.com");
+    expect(wrapper.props().content.title).toBe("Google");
+    expect(wrapper.props().content.icon).toBe("");
   });
 });
