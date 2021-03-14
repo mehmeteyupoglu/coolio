56e022235b512d73a8b0d28e03828106685b15fa
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2021-03-14 19:29:06 +0300

    test: more testing on logo-group

--- tests/unit/components/LogoGroup.spec.js
+++ tests/unit/components/LogoGroup.spec.js
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import { LogoGroup } from "../../../src/components/molecules";
 
 describe("LogoGroup", () => {
-  it("renders a message and responds correctly to user input", () => {
+  it("should render a logo group with props given", () => {
     const wrapper = mount(LogoGroup, {
       propsData: {
         content: {
@@ -19,5 +19,23 @@ describe("LogoGroup", () => {
       "https://uploads-ssl.webflow.com/5d7ab80fa9740ce01c57ee9d/5f2978019586604c1c53aa87_toolio-icon-64.svg"
     );
     expect(wrapper.props().content.title).toBe("Toolio");
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("should render a logo group with props given", () => {
+    const wrapper = mount(LogoGroup, {
+      propsData: {
+        content: {
+          src: "",
+
+          title: "Coolio",
+        },
+      },
+    });
+
+    // see if the props are rendered correctly
+    expect(wrapper.props().content.src).toBe("");
+    expect(wrapper.props().content.title).toBe("Coolio");
+    expect(wrapper.element).toMatchSnapshot();
   });
 });
